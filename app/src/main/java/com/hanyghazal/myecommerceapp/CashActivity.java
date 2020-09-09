ca94af15f9660a3619d9add711d86a817987c1dc
hanyghazal79 <dr.hanyghazal@gmail.com>
2020-09-10 00:47:38 +0200

    Created Cash on delivery activity

---
+++ app/src/main/java/com/hanyghazal/myecommerceapp/CashActivity.java
@@ -0,0 +1,14 @@
+package com.hanyghazal.myecommerceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cash);
+    }
+}
